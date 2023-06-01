8bd64cf04cc72e057ad9499453926fd68e1602ab
Damian <damianszymczak01@wp.pl>
2023-06-01 08:54:24 +0200

    our new message

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path,include
-from .views import BlogListView
+from .views import BlogListView, BlogDetailView
 urlpatterns = [
-    path("",BlogListView.as_view(), name="home")
+    path("",BlogListView.as_view(), name="home"),
+    path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail")
 ]
